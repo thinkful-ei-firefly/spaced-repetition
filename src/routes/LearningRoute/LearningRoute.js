1452d531810be7c6d580b45df1c8bcd0a2c6e648
Keith Prince <keithcprince@gmail.com>
2019-10-17 11:30:32 -0400

    submit form

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -6,7 +6,8 @@ class LearningRoute extends Component {
   state = {
     correct: 0,
     incorrect: 0,
-    currentWord: ''
+    currentWord: '',
+    totalScore: 0
   };
 
   getLanguageHead = async () => {
@@ -21,7 +22,8 @@ class LearningRoute extends Component {
     this.setState({
       currentWord: data.nextWord,
       correct: data.wordCorrectCount,
-      incorrect: data.wordIncorrectCount
+      incorrect: data.wordIncorrectCount,
+      totalScore: data.totalScore
     });
   };
 
@@ -29,21 +31,25 @@ class LearningRoute extends Component {
     this.getLanguageHead();
   }
 
-  renderWord(){
-    return <section>
-      <h2>{this.state.currentWord}</h2>
-      <p>Guesses: {this.state.correct + this.state.incorrect}</p>
-      <p>Correct: {this.state.correct}</p>
-    </section>    
+  onSubmit = event => {
+    event.preventDefault()
+    let {guess} = event.target
+    guess = guess.value
   }
 
   render() {
     return <section>
-        <h3>Word to practice</h3>
-        {this.renderWord()}
-        <a href="/learn">
-          <button>Start practicing</button>
-        </a>
+        <h2>Translate the word:</h2>
+        <span>{this.state.currentWord}</span>
+        <p>Your total score is: {this.state.totalScore}</p>
+        <p>You have answered this word correctly {this.state.correct} times.</p>
+        <p>You have answered this word incorrectly {this.state.incorrect} times.</p>
+        
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="learn-guess-input">What's the translation for this word?</label>
+          <input type="text" required id="learn-guess-input" name="guess"></input>
+          <button type="submit">Submit your answer</button>
+        </form>
     </section>;
   }
 }
---
+++ src/services/guess-services.js
@@ -0,0 +1,9 @@
+import config from '../config'
+
+const guessService = {
+  submitGuess(guess){
+
+  }
+}
+
+module.exports = guessService
