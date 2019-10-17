d9a5c304318241f70a5025ce2f356c35ad6779e8
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-10-17 12:17:38 -0400

    working post guess form

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,56 +1,103 @@
-import React, { Component } from "react";
-import TokenService from "../../services/token-service";
-import config from "../../config";
+import React, { Component } from 'react';
+import GuessService from '../../services/guess-services';
 
 class LearningRoute extends Component {
   state = {
     correct: 0,
     incorrect: 0,
     currentWord: '',
-    totalScore: 0
-  };
-
-  getLanguageHead = async () => {
-    const data = await fetch(`${config.API_ENDPOINT}/language/head`, {
-      headers: {
-        authorization: `Bearer ${TokenService.getAuthToken()}`
-      }
-    }).then(res =>
-      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
-    );
-    console.log(data)
-    this.setState({
-      currentWord: data.nextWord,
-      correct: data.wordCorrectCount,
-      incorrect: data.wordIncorrectCount,
-      totalScore: data.totalScore
-    });
+    totalScore: 0,
+    nextWord: '',
+    wordCorrectCount: 0,
+    wordIncorrectCount: 0,
+    answer: '',
+    guess: '',
+    isCorrect: null
   };
 
   componentDidMount() {
-    this.getLanguageHead();
+    GuessService.getLanguageHead().then(res =>
+      this.setState({
+        currentWord: res.nextWord,
+        correct: res.wordCorrectCount,
+        incorrect: res.wordIncorrectCount,
+        totalScore: res.totalScore
+      })
+    );
   }
 
   onSubmit = event => {
-    event.preventDefault()
-    let {guess} = event.target
-    guess = guess.value
-  }
+    event.preventDefault();
+    let { guess } = event.target;
+    guess = guess.value;
+    this.setState({ guess });
+    GuessService.submitGuess(guess).then(res => {
+      guess = '';
+      const {
+        nextWord,
+        wordCorrectCount,
+        wordIncorrectCount,
+        answer,
+        totalScore,
+        isCorrect
+      } = res;
+      this.setState({
+        nextWord,
+        wordCorrectCount,
+        wordIncorrectCount,
+        answer,
+        totalScore,
+        isCorrect
+      });
+    });
+  };
+
+  renderForm = () => {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <label htmlFor="learn-guess-input">
+          What's the translation for this word?
+        </label>
+        <input type="text" required id="learn-guess-input" name="guess"></input>
+        <button type="submit">Submit your answer</button>
+      </form>
+    );
+  };
+
+  renderResponse = () => {
+    return (
+      <>
+        <h2>
+          {this.state.isCorrect === true
+            ? 'You were correct! :D'
+            : 'Good try, but not quite right :('}
+        </h2>
+        <p className="DisplayFeedback">
+          The correct translation for {this.state.currentWord} was
+          {this.state.answer} and you chose {this.state.guess}!
+        </p>
+        <button>Try another word!</button>
+      </>
+    );
+  };
 
   render() {
-    return <section>
-        <h2>Translate the word:</h2>
+    return (
+      <section>
+        {this.state.isCorrect === null ? <h2>Translate the word:</h2> : ''}
         <span>{this.state.currentWord}</span>
-        <p>Your total score is: {this.state.totalScore}</p>
+        <p className="DisplayScore">
+          Your total score is: {this.state.totalScore}
+        </p>
         <p>You have answered this word correctly {this.state.correct} times.</p>
-        <p>You have answered this word incorrectly {this.state.incorrect} times.</p>
-        
-        <form onSubmit={this.onSubmit}>
-          <label htmlFor="learn-guess-input">What's the translation for this word?</label>
-          <input type="text" required id="learn-guess-input" name="guess"></input>
-          <button type="submit">Submit your answer</button>
-        </form>
-    </section>;
+        <p>
+          You have answered this word incorrectly {this.state.incorrect} times.
+        </p>
+        {this.state.isCorrect === null
+          ? this.renderForm()
+          : this.renderResponse()}
+      </section>
+    );
   }
 }
 
--- src/services/guess-services.js
+++ src/services/guess-services.js
@@ -1,9 +1,28 @@
-import config from '../config'
+import config from '../config';
+import TokenService from './token-service';
 
-const guessService = {
-  submitGuess(guess){
-    
+const GuessService = {
+  submitGuess(guess) {
+    return fetch(`${config.API_ENDPOINT}/language/guess`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        authorization: `Bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify({ guess })
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
+  },
+  getLanguageHead() {
+    return fetch(`${config.API_ENDPOINT}/language/head`, {
+      headers: {
+        authorization: `Bearer ${TokenService.getAuthToken()}`
+      }
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
   }
-}
+};
 
-module.exports = guessService
+export default GuessService;
