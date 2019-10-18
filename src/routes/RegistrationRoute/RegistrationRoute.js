d9a1e353597ec4f097990a0488cbe649e54cf9eb
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-10-18 12:43:52 -0400

    begin refactor css files

--- src/routes/RegistrationRoute/RegistrationRoute.js
+++ src/routes/RegistrationRoute/RegistrationRoute.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
-import "../register.css";
+import React, { Component } from 'react';
+import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
+import './RegistrationRoute.css';
 
 class RegistrationRoute extends Component {
   static defaultProps = {
@@ -16,7 +16,7 @@ class RegistrationRoute extends Component {
 
   render() {
     return (
-      <section>
+      <section className="Registration">
         <p>
           Practice learning a language with the spaced reptition revision
           technique.
