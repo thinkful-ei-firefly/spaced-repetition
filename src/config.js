943180f82b49f4c76bf5d89079a38368b83f09e0
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-10-18 10:53:09 -0400

    prepare for deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
-}
+  API_ENDPOINT: process.env.REACT_APP_API_BASE || 'http://localhost:8000/api',
+  REACT_APP_API_BASE:
+    process.env.REACT_APP_API_BASE || 'http://localhost:8000/api',
+  TOKEN_KEY: 'spaced-repetition-client-auth-token'
+};
