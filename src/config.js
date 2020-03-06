6d22d6e9acd352028877a26dcd4548d61adfb7c6
Ben Rosen <benjamin.j.rosen@gmail.com>
2020-03-05 22:10:57 -0500

    api url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_BASE || 'http://localhost:8000/api',
+  API_ENDPOINT: process.env.REACT_APP_API_BASE || 'https://spaced-repetition-japanese.herokuapp.com/api',
   REACT_APP_API_BASE:
-    process.env.REACT_APP_API_BASE || 'http://localhost:8000/api',
+    process.env.REACT_APP_API_BASE || 'https://spaced-repetition-japanese.herokuapp.com/api',
   TOKEN_KEY: 'spaced-repetition-client-auth-token'
 };
